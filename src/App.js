d4bc048054920f3d6c6b5b2a88d433e41f7939e2
unknown <mareklayman@gmail.com>
2020-10-27 11:42:38 +0100

    infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,61 +1,40 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 import "./App.scss";
 import Header from "./components/Header";
 import Search from "./components/Search";
 import Gallery from "./components/Gallery";
 import DataError from "./components/DataError";
+import Loader from "./components/Loader";
 
 function App() {
   const [photos, setPhotos] = useState([]);
-  const [query, setQuery] = useState([""]);
-  const [page, setPage] = useState(1);
-
-  const loaderRef = useRef();
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+  const [hasMorePages, setHasMorePages] = useState(false);
 
   useEffect(() => {
-    setData(query);
+    fetchData(query);
   }, [query]);
 
-  useEffect(() => {
-    window.addEventListener("scroll", showMorePictures);
-  }, [page, query]);
-
-  const showMorePictures = async (e) => {
-    const {
-      scrollTop,
-      scrollHeight,
-      clientHeight,
-    } = e.path[1].document.documentElement;
-
-    if (scrollTop + clientHeight >= scrollHeight - 1) {
-      showLoading();
-    }
-  };
-
-  const showLoading = () => {
-    const loader = loaderRef.current.classList;
-    loader.add("open");
-
-    setTimeout(() => {
-      loader.remove("open");
-
-      setTimeout(() => {
-        setPage(page + 1);
-        setData(query);
-      }, 300);
-    }, 1000);
-  };
-
-  const setData = async (queryWord) => {
+  const fetchData = async (queryWord) => {
     const api = "https://api.unsplash.com/search/photos";
-    const clietId = "2kNZqP1DiER5-WGIGGzPtuv5oVJRriAUzZvqznmFtRM";
+    const accessKey = process.env.REACT_APP_ACCESSKEY;
 
     try {
+      console.log(query);
       const res = await fetch(
-        `${api}?page=${page}&query=${queryWord}&per_page=10&client_id=${clietId}`
+        `${api}?page=${pageNumber}&query=${queryWord}&per_page=10&client_id=${accessKey}`
       );
+      console.log(res);
       const data = await res.json();
+
       setPhotos([...photos, ...data.results]);
+      console.log(data.total_pages);
+      setHasMorePages(data.total_pages > 1)
+        ? setPageNumber(pageNumber + 1)
+        : setPageNumber(1);
     } catch (error) {
       console.log(error);
     }
@@ -66,13 +45,14 @@ function App() {
       <Header />
       <Search query={query} setQuery={setQuery} />
       <DataError />
-      <Gallery photos={photos} />
-      <div ref={loaderRef} className="lds-ellipsis">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+      <InfiniteScroll
+        dataLength={photos.length}
+        next={fetchData}
+        hasMore={hasMorePages}
+        loader={<Loader />}
+      >
+        <Gallery photos={photos} />
+      </InfiniteScroll>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Liked from "./Liked";
 const Header = () => {
   return (
     <div className="header">
-      <h1>photoGallery</h1>
+      <h1>#photoGallery</h1>
       <div className="my-panel">
         <Liked />
       </div>
